f979f1991077b1e5b74d3b1fc1ca9297b7bc7f21
sonyabot <sonya.jilani@gmail.com>
2022-11-19 10:42:53 -0500

    Update app.php
    Add some padding

--- app.php
+++ app.php
@@ -3,6 +3,6 @@
 require_once('SecretSanta.php');
 
 $api_key = (string)readline('API Key: ');
-$file_path = (string)readline('Enter your santa list filepath');
+$file_path = (string)readline('Enter your santa list filepath: ');
 $santa = new SecretSanta($api_key, $file_path);
 $santa->run();
